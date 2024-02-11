c08b9362d54b56264226bf824288453e7eedf0a3
pv42 <pv42.97@gmail.com>
2024-02-11 16:04:58 +0100

    update bootstrap version to 5.3.2

--- packages/yew-bootstrap/src/util/include.rs
+++ packages/yew-bootstrap/src/util/include.rs
@@ -5,7 +5,7 @@ use yew::{html, virtual_dom::VNode};
 pub fn include_cdn() -> VNode {
     html! {
         <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
             rel="stylesheet"
             integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
             crossorigin="anonymous"
@@ -17,9 +17,9 @@ pub fn include_cdn() -> VNode {
 pub fn include_cdn_js() -> VNode {
     html! {
         <>
-            <link data-trunk={"true"} rel="copy-file" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js.map" />
+            <link data-trunk={"true"} rel="copy-file" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js.map" />
             <script
-                src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+                src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
                 data-trunk={"true"}
                 integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
                 crossorigin="anonymous"
@@ -33,7 +33,7 @@ pub fn include_cdn_js() -> VNode {
 pub fn include_inline() -> VNode {
     html! {
         <style>
-            {include_str!("bootstrap-5.1.3.min.css")}
+            {include_str!("bootstrap-5.3.2.min.css")}
         </style>
     }
 }
